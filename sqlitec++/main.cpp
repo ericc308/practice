24382c773a8aa8d6b427ebece847a2c42c5a3804
ycj <zerogod708705@hotmail.com>
2020-02-24 14:53:15 +0800

    order limit

--- sqlitec++/main.cpp
+++ sqlitec++/main.cpp
@@ -82,18 +82,12 @@ int main() {
   update_to_database(user2, storage);
   User user3{1, "Ken", 3};
   add_data_to_database(user3, storage);
-  User user4{1, "Penaa", 3};
-  User user5{1, "Yen", 5};
-  User user6{1, "Fenff", 2};
-  User user7{1, "Gen", 1};
-  std::vector<User> user_list;
-  user_list.push_back(user4);
-  user_list.push_back(user5);
-  user_list.push_back(user6);
-  user_list.push_back(user7);
+  std::vector<User> user_list = {{1, "Penaa", 3}, {1, "Yen", 5}, {1, "Fenff", 2}, {1, "Gen", 1}, {1, "Zenni", 1}, {1, "Xoen", 1}, {1, "Benna", 1}};
+
   add_datagroup_to_database(user_list, storage);
 
   // print all
+  std::cout << "Print all data in database. \n";
   std::vector<User> allUsers = storage.get_all<User>();
   std::cout << "allUsers (" << allUsers.size() << "):" << std::endl;
   for (int item = 0; item < allUsers.size(); item++) {
@@ -101,37 +95,55 @@ int main() {
   }
 
   // select 1 and show all
+  std::cout << "Get_all @ condition. \n";
   std::vector<User> SelectUsers = storage.get_all<User>(where(c(&User::typeId) > 2));
   std::cout << "User typeId > 2 (" << SelectUsers.size() << "):" << std::endl;
   for (auto &iUser : SelectUsers) {
     std::cout << storage.dump(iUser) << std::endl;
   }
 
-  //select by execute sql command
+  // select order by asc
+  std::cout << "Get_all @ condition with  order in asc. \n";
+  auto select_asc = storage.get_all<User>(where(length(&User::firstName) > 3), order_by(&User::id).asc());
+  std::cout << "Get_all @ condition(length(name)>3)order by id asc " << select_asc.size() << std::endl;
+  for (auto &iUser : select_asc) {
+    std::cout << storage.dump(iUser) << std::endl;
+  }
+
+  // select order by desc
+  std::cout << "Get_all @ condition(length(name)>3) with  order in desc. \n";
+  auto select_desc = storage.get_all<User>(where(length(&User::firstName) > 3), order_by(&User::id).desc());
+  std::cout << "where length( firstname > 3 ) order by id desc " << select_desc.size() << std::endl;
+  for (auto &iUser : select_desc) {
+    std::cout << storage.dump(iUser) << std::endl;
+  }
+
+  // select order and limit(the number of showing)
+  std::cout << "Get_first 3 data @ condition(length(name)>3) with  order in desc. \n";
+  auto select_desc_limit3 = storage.get_all<User>(where(length(&User::firstName) > 3), order_by(&User::id).desc(), limit(3));
+  for (auto &iUser : select_desc_limit3) {
+    std::cout << storage.dump(iUser) << std::endl;
+  }
+  std::cout << "Get 4th, 5th and 6th data @ condition(length(name)>3) with  order in desc. \n";
+  auto select_desc_limit35 = storage.get_all<User>(where(length(&User::firstName) > 3), order_by(&User::id).desc(), limit(3, 5));
+  for (auto &iUser : select_desc_limit35) {
+    std::cout << storage.dump(iUser) << std::endl;
+  }
+  std::cout << "Get first 3 data after 2nd data @ condition(length(name)>3) with  order in desc. \n";
+  auto select_desc_offset2 = storage.get_all<User>(where(length(&User::firstName) > 3), order_by(&User::id).desc(), limit(3, offset(2)));
+  for (auto &iUser : select_desc_offset2) {
+    std::cout << storage.dump(iUser) << std::endl;
+  }
+
+  // select by execute sql command
+  std::cout << "Use prepare statement. \n";
   auto selectStatement = storage.prepare(select(&User::firstName, where(length(&User::firstName) > 3)));
-  std::cout << "selectStatement = " << selectStatement.sql() << std::endl;  //  prints "SELECT doctor_id FROM ..."
-  auto userrows = storage.execute(selectStatement); //  rows is std::vector<decltype(Visit::doctor_id)>
+  std::cout << "Command = " << selectStatement.sql() << std::endl; //  prints "SELECT doctor_id FROM ..."
+  auto userrows = storage.execute(selectStatement);                //  rows is std::vector<decltype(Visit::doctor_id)>
 
-     for (int iUser=0;iUser<userrows.size();iUser++) {
+  for (int iUser = 0; iUser < userrows.size(); iUser++) {
     std::cout << userrows[iUser] << std::endl;
-     }
-
-//select order by desc
-  std::cout << "where length( firstname > 3 ) order by id desc" << std::endl; 
-    auto select_desc = storage.get_all<User>(where(length(&User::firstName) > 3),order_by(&User::id).desc());
-    for (auto &iUser : select_desc) {
-      std::cout << storage.dump(iUser) << std::endl;
-    }
-
-    //select order by asc
-    std::cout << "where length( firstname > 3 ) order by id asc" << std::endl; 
-    auto select_asc = storage.get_all<User>(where(length(&User::firstName) > 3),order_by(&User::id).asc());
-    for (auto &iUser : select_asc) {
-      std::cout << storage.dump(iUser) << std::endl;
-    }
-
-
-
+  }
 
   return 0;
 }
